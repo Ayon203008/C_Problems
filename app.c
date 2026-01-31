fc48db0152d214b9fe81df9bc654adcdc20f26a8
Ayon203008 <paypal572874@gmail.com>
2026-01-31 12:22:56 +0600

    dsf

--- app.c
+++ app.c
@@ -46,3 +46,55 @@
         
 //     }
 // }
+
+// reverse of an array
+
+// #include <stdio.h>
+
+// int main() {
+//     int arr[5] = {10, 20, 30, 40, 50};
+//     int n = 5;
+
+//     printf("Reversed array: ");
+//     for (int i = n - 1; i >= 0; i--) {
+//         printf("%d ", arr[i]);
+//     }
+
+//     return 0;
+// }
+
+
+// reverse of an array
+
+#include <stdio.h>
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int n=5; // here n = total elements of an array
+    for(int i=n-1;i>=0;i--){
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
