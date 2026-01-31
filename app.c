50a143acf34f8a6337411a7e7d5b1d92a57fb2bc
Ayon203008 <paypal572874@gmail.com>
2026-01-31 12:18:52 +0600

    ABCD

--- app.c
+++ app.c
@@ -1,149 +1,48 @@
 // printing numbers in an array
-// #include <stdio.h>
-// int main(){
-//     int arr[5]={1,2,3,4,5};
-//     for(int i=0;i<5;i++){
-//         printf("%d ",arr[i]);
-//     }
-//     return 0;
-// }
-
-
-
-// take input the an array and print them
 
 // #include <stdio.h>
 // int main(){
 //     int arr[5];
-//     printf("Enter your number : ");
+//     printf("Enter 5 numbers : ");
 //     for(int i=0;i<5;i++){
 //         scanf("%d",&arr[i]);
 //     }
+//     printf("The numbers are : ");
 //     for(int i=0;i<5;i++){
 //         printf("%d",arr[i]);
 //     }
-//     return 0;
 // }
 
 
-
-// print those values that are greater than 15
-
-// #include <stdio.h>
-// int main(){
-//     int arr[5]={12,13,14,15,16};
-//     for(int i=0;i<5;i++){
-//         if(arr[i]>15){
-//             printf("%d",arr[i]);
-//         }
-//     }
-//     return 0;
-// }
-
-
-
-
-// searching numbers in an array
-
-// summation of an array 
-// #include <stdio.h>
-// int main(){
-//     int arr[5]={10,120,102,30,12};
-//     int sum=0;
-//     for(int i=0;i<5;i++){
-//         sum=sum+arr[i];
-//     }
-//     printf("Total summation = %d",sum);
-// }
-
-
-// Find maximum number in an array
-// #include <stdio.h>
-// int main(){
-//     int arr[5]={10,120,102,30,12};
-//     int max=arr[0];
-//     for(int i=0;i<5;i++){
-//         if(arr[i]>max){
-//             max=arr[i];
-//             break;
-//         }
-//     }
-//     printf("Maximum Number = %d",max);
-// }
-
-
-// searching number in an array
+// find the maximum number in an array
 
 // #include <stdio.h>
 // int main(){
 //     int arr[5]={1,2,3,4,5};
-//     int number=4;
+//     int max=arr[0];
+    
 //     for(int i=0;i<5;i++){
-//         if(number==arr[i]){
-//             printf("The number is find and it is %d ",arr[i]);
-//             break; // ! If you dont use break it will continue and you will not get the result
+//         if(arr[i]>max){
+//             max=arr[i];
 //         }
 //     }
+//     printf("%d",max);
 // }
 
+// searchign number in an array
 
-
-// take input as chracter and print them 
 
 // #include <stdio.h>
 // int main(){
-//     char name[30];
-//     printf("Enter your name : ");
-//     gets(name);
-//     printf("%s",name);
-//     return 0;
+//     int arr[5]={1,2,3,4,5};
+//     int number=5;
+    
+//     for(int i=0;i<5;i++){
+//         if(number==arr[i]){
+
+//             printf("%d",arr[i]);
+//             break; // ! must use break
+//         }
+        
+//     }
 // }
-
-
-
-// concatination of atring
-
-#include <stdio.h>
-#include<string.h>
-int main(){
-    char name1[100]="Abid";
-    char name2[20]=" Hasan";
-    strcat(name1,name2);
-    puts(name1); 
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
